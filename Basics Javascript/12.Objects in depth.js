59032b6a41d165bc3251a3b0f07e3c8175081041
Shub202 <info.shubhamkumar2001@gmail.com>
2024-07-09 01:52:53 +0530

    New Learning Obj. in Js

---
+++ Basics Javascript/12.Objects in depth.js
@@ -0,0 +1,13 @@
+//Singleton -Constructs
+
+//object literals 
+
+const JsUser = {
+    name: "Ram",
+    age : 18,
+    email: "ram123@gmail.com",
+    lastlogindays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
