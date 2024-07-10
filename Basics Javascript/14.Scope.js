254139c39a5dce51ae45acf6d52f59195f92f194
Shub202 <info.shubhamkumar2001@gmail.com>
2024-07-10 17:32:22 +0530

    New learning about scope in js

--- Basics Javascript/14.Scope.js
+++ Basics Javascript/14.Scope.js
@@ -16,3 +16,42 @@ console.log(a);
 //console.log(b);
 //console.log(c);
 
+
+//Nested loop//
+
+function one (){
+    const username ="Lallu "
+     function two (){
+        const Website ="Youtube"
+        console.log(username);
+     }
+     //console.log(Website);
+     two()
+}
+//one()luel
+
+
+if (true) {
+    const username = "hari"
+    if (username === "hari") {
+        const Website = "youtube"
+        console.log(username + Website);
+    }
+    //console.log(Website);
+}
+//console.log(username);
+
+
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+
+
+console.log(addtwo(5));     //Error => because declaration  of value is not correct 
+const addtwo = function(num){
+    return num + 2
+}
