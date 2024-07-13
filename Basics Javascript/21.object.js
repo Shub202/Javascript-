749a0e17001dc5fb9dfa1207ab1f2eff87d09028
Shub202 <info.shubhamkumar2001@gmail.com>
2024-07-14 00:00:46 +0530

    New learning objects in JS

--- Basics Javascript/21.object.js
+++ Basics Javascript/21.object.js
@@ -7,6 +7,52 @@ const myobject = {
 }
 
 for (const key in myobject) {
-    console.log(`${key} shortcut is for ${myobject[key]}`);
+    //console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+const programming = ["js", "rb", "py","java","cpp"]
+
+for (const key in programming) {
+    //console.log(key);
+    console.log(programming[key]);
+   
 }
     
+//*************************For EACH LOOP*******************//
+const coding = ["js", "rb", "python","java","cpp"]
+
+//coding.forEach(  function (val) {
+            //console.log(val);
+//})
+
+//coding.forEach( (item) => {
+    //console.log(item);
+//})
+
+//function printMe(item){
+     //console.log(item);
+//}
+
+//coding.forEach(printMe)
+
+coding.forEach((item,index,arr)=>{
+    console.log(item,index,arr);
+})
+
+const mycoding = [
+    {
+        languagename:"javascript",
+         languageFilename :"js"
+        },
+        {
+            languagename:"java",
+             languageFilename :"java"
+            },
+            {
+                languagename:"python",
+                 languageFilename :"py"
+                },
+]
+mycoding.forEach((item)=>{
+    console.log(item.languagename);
+})
